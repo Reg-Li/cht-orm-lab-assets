e3069c3760ef949384c7e4a25cd930ef1ae22382
Hua-Hsing Li <fare.prince@gmail.com>
2016-01-29 10:51:08 +0800

    DB seeding with random data(use Faker).

---
+++ database/seeds/PostTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Post;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::truncate();
+
+        $faker = \Faker\Factory::create('zh_TW');
+
+        foreach(range(15, 1) as $number) {
+            Post::create([
+                'title' => $faker->sentence,
+                'content' => $faker->paragraph,
+                'is_feature' => rand(0, 1),
+                'created_at' => Carbon::now()->subDays($number),
+                'updated_at' => Carbon::now()->subDays($number)
+            ]);
+        }
+    }
+}
